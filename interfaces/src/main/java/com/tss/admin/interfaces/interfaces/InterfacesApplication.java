d11bc69e5a7b7b3bed36084decac5c0ddee63692
liutong <135225170@qq.com>
2018-12-04 17:15:34 +0800

    admin init

---
+++ interfaces/src/main/java/com/tss/admin/interfaces/interfaces/InterfacesApplication.java
@@ -0,0 +1,12 @@
+package com.tss.admin.interfaces.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfacesApplication.class, args);
+	}
+}
